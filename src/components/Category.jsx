5f5497ee5629102850122680d344c85510dcdf11
geleg <gerard.leganovic@gmail.com>
2023-12-06 16:34:02 +0200

    v.044

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,78 +1,93 @@
+// Import necessary modules from React and Axios library
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// Functional component named Category
 const Category = () => {
+    // API key for accessing the MovieDB API
     const apiRaktas = '&api_key=53c258bb52d305146e19a71e58aa2cc5';
 
-    const [movie, setMovie] = useState([]);
-    const [error, setError] = useState(null);
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const [genres, setGenres] = useState([]);
+    // State variables using the 'useState' hook to manage component state
+    const [movie, setMovie] = useState([]); // Stores movie data
+    const [error, setError] = useState(null); // Manages error states
+    const [selectedCategory, setSelectedCategory] = useState(''); // Tracks the selected movie category
+    const [genres, setGenres] = useState([]); // Stores movie genres
 
+    // Function to fetch movie genres from the MovieDB API
     const fetchMovieGenres = async () => {
         try {
             const response = await axios.get(
                 `https://api.themoviedb.org/3/genre/movie/list?language=en-US${apiRaktas}`
             );
-            setGenres(response.data.genres);
+            setGenres(response.data.genres); // Sets the fetched genres into the state
         } catch (error) {
-            console.error('Error fetching genres:', error);
+            console.error('Error fetching genres:', error); // Logs an error if fetching fails
         }
     };
 
+    // useEffect hook to fetch movie genres once when the component mounts
     useEffect(() => {
         fetchMovieGenres();
-    }, []); // Fetch genres when component mounts
+    }, []); // Empty dependency array ensures this effect runs only once on mount
 
+    // Event handler to update the selected category when the dropdown selection changes
     const handleCategoryChange = (event) => {
-        setSelectedCategory(event.target.value);
+        setSelectedCategory(event.target.value); // Updates the selected category state
     };
 
+    // Function to fetch movies based on the selected category
     const fetchMoviesByCategory = (category) => {
         axios
             .get(`https://api.themoviedb.org/3/discover/movie?with_genres=${category}&language=en-US${apiRaktas}`)
             .then((response) => {
-                setError(null);
-                setMovie(response.data.results);
+                setError(null); // Clears any previous errors
+                setMovie(response.data.results); // Sets the fetched movies into the state
             })
             .catch((error) => {
-                setError(error);
-                setMovie([]);
+                setError(error); // Sets an error if fetching movies fails
+                setMovie([]); // Clears movie data in case of an error
             });
     };
 
+    // useEffect hook to fetch movies when a category is selected
     useEffect(() => {
         if (selectedCategory) {
-            fetchMoviesByCategory(selectedCategory);
+            fetchMoviesByCategory(selectedCategory); // Fetches movies based on the selected category
         }
-    }, [selectedCategory]);
+    }, [selectedCategory]); // Runs when the selected category changes
 
+    // Component return: renders JSX containing the UI elements
     return (
         <>
             <h1>Search Movie by Category</h1>
+            {/* Dropdown to select movie category */}
             <div>
                 <select value={selectedCategory} onChange={handleCategoryChange}>
                     <option value="">Select a category</option>
                     {genres.map((genre) => (
+                        // Maps through available genres to populate dropdown options
                         <option key={genre.id} value={genre.id}>
                             {genre.name}
                         </option>
                     ))}
                 </select>
             </div>
+            {/* Display movies based on selected category */}
             <div>
-                {movie.length > 0 ? (
+                {movie.length > 0 ? ( // Checks if movies are available
                     <ul className='movieList'>
                         {movie.map((movieItem) => (
+                            // Maps through fetched movies to display details
                             <li key={movieItem.id} className='movieInfo'>
                                 <p className='title'>{movieItem.original_title}</p>
-                                <img src={`https://image.tmdb.org/t/p/w200${movieItem.poster_path}`} alt="posteris" />
+                                <img src={`https://image.tmdb.org/t/p/w200${movieItem.poster_path}`} alt="poster" />
                                 <p>Release date: {movieItem.release_date}</p>
                                 <p>Vote: {movieItem.vote_average}</p>
                             </li>
                         ))}
                     </ul>
                 ) : (
+                    // Displays a message if no movies are found or fetched yet
                     <p>No movies found or search yet.</p>
                 )}
             </div>
@@ -80,4 +95,4 @@ const Category = () => {
     );
 };
 
-export default Category;
+export default Category; // Exporting the Category component as default
--- src/components/SearchCategory.jsx
+++ src/components/SearchCategory.jsx
@@ -1,64 +1,63 @@
-import React, { useState, useEffect, useRef } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 
-
+// Define a functional component named Search
 const Search = () => {
+    // API key for accessing the MovieDB API
+    const apiRaktas = '&api_key=53c258bb52d305146e19a71e58aa2cc5';
 
+    // State variables to manage component state
+    const [movie, setMovie] = useState([]); // Stores movie data
+    const [error, setError] = useState(null); // Manages error states
 
-    const apiRaktas = '&api_key=53c258bb52d305146e19a71e58aa2cc5'
-
-    const [movie, setMovie] = useState([]);
-    const [error, setError] = useState(null);
-
-
+    // Function to fetch movies from the MovieDB API
     const fetchMovies = () => {
-
         axios
             .get(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc${apiRaktas}`)
             .then((response) => {
-                setError(null);
-                setMovie(response.data.results.slice(0, 10));
+                setError(null); // Clears any previous errors
+                setMovie(response.data.results.slice(0, 10)); // Stores the first 10 movies from the fetched data
             })
             .catch((error) => {
-                setError(error);
-                setMovie([]);
+                setError(error); // Sets an error if fetching movies fails
+                setMovie([]); // Clears movie data in case of an error
             });
-
     };
 
+    // useEffect hook to fetch movies once when the component mounts
     useEffect(() => {
         fetchMovies();
-    }, []);
+    }, []); // Empty dependency array ensures this effect runs only once on mount
 
+    // Component return: renders JSX containing the UI elements
     return (
-
         <>
             <h1>Most popular movies</h1>
 
             <div>
-                <div >
+                <div>
+                    {/* Conditional rendering based on movie data availability */}
                     {movie.length > 0 ? (
+                        // Renders a list of movies if available
                         <ul className='movieList'>
                             {movie.map((movie) => (
-                                <li className='movieInfo'>
+                                // Maps through fetched movies to display details
+                                <li className='movieInfo' key={movie.id}>
                                     <p className='title'>{movie.original_title}</p>
-                                    <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="posteris" />
+                                    <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="poster" />
                                     <p>Release date: {movie.release_date}</p>
                                     <p>Vote: {movie.vote_average}</p>
-
-
                                 </li>
                             ))}
                         </ul>
                     ) : (
+                        // Displays a message if no movies are fetched yet
                         <p>No search yet.</p>
                     )}
                 </div>
-
-
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Search
+export default Search; // Exporting the Search component as default
